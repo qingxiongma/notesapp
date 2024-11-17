e9a5abc274604682df482dbdbd38e9c4ef793318
Qingxiong Ma <qma@ucmo.edu>
2024-11-16 21:50:42 -0600

    the notes app

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,10 @@
+﻿import { defineStorage } from "@aws-amplify/backend";
+
+export const storage = defineStorage({
+  name: "amplifyNotesDrive",
+  access: (allow) => ({
+    "media/{entity_id}/*": [
+      allow.entity("identity").to(["read", "write", "delete"]),
+    ],
+  }),
+});
